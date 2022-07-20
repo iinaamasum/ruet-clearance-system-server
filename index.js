be3714e0196a53697cb333a6d242e03adf9b75a9
iinaamasum <iinaamasum@gmail.com>
2022-07-20 08:00:18 +0600

    mongo db connected with profile details route

--- index.js
+++ index.js
@@ -4,10 +4,50 @@ require('dotenv').config();
 
 const port = process.env.PORT || 5000;
 const app = express();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 //middle_ware
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bps1i.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const studentCollection = client
+      .db('studentCollection')
+      .collection('studentData');
+
+    /**
+     * student information post
+     * link: http://localhost:5000/studentDetails
+     */
+    app.post('/studentDetails', async (req, res) => {
+      const data = req.body;
+      const result = await studentCollection.insertOne(data);
+      console.log(result);
+      res.send(result);
+    });
+    /**
+     * get student information
+     * link: http://localhost:5000/studentDetails?email=${email}
+     */
+    app.get('/studentDetails', async (req, res) => {
+      const query = { email: req.query.email };
+      const info = await studentCollection.findOne(query);
+      res.send(info);
+    });
+  } finally {
+  }
+}
+
+run().catch(console.dir);
 
 //server_test
 app.get('/', async (req, res) => {
