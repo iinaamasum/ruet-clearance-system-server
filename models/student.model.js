ddf2bf99f1c15fd7d932ac6e94147c7173ae792a
iinaamasum <iinaamasum@gmail.com>
2022-09-25 06:53:43 +0600

    student model added

--- models/student.model.js
+++ models/student.model.js
@@ -1,44 +1,73 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
 
-const studentSchema = mongoose.Schema({
-  name: {
-    type: String,
-    required: {
-      value: true,
-      message: 'Student Name must be given.',
+const studentSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: {
+        value: true,
+        message: 'Student Name must be given.',
+      },
+      trim: true,
+      lowercase: true,
+      minLength: [2, 'Student Name can be at least 2 char.'],
+      maxLength: [100, "Student Name can't exceed 100 chars."],
     },
-    trim: true,
-    lowercase: true,
-    minLength: [2, 'Student Name can be at least 2 char.'],
-    maxLength: [100, "Student Name can't exceed 100 chars."],
+    email: {
+      type: String,
+      required: {
+        value: true,
+        message: 'Email address is required',
+      },
+      validate: {
+        validator: [validator.isEmail, 'Please provide a valid mail.'],
+      },
+    },
+    faculty: {
+      type: String,
+      required: {
+        value: true,
+        message: 'Faculty data is required',
+      },
+      trim: true,
+      lowercase: true,
+    },
+    dept: {
+      type: String,
+      required: {
+        value: true,
+        message: 'Dept data is not provided yet',
+      },
+      trim: true,
+      uppercase: true,
+    },
+    series: {
+      type: String,
+      required: {
+        value: true,
+        message: 'Series info not found.',
+      },
+    },
+    roll: {
+      type: String,
+      required: {
+        value: true,
+        message: 'Roll must be provided.',
+      },
+    },
+    contact_number: {
+      type: String,
+      required: {
+        value: true,
+        message: 'Please provide contact number.',
+      },
+    },
   },
-  email: {
-    type: String,
-    required: {
-      value: true,
-      message: 'Email address is required',
-    },
-    validate: {
-      validator: (value) => {},
-    },
-  },
-  faculty: {
-    type: String,
-    required: {
-      value: true,
-      message: 'Faculty data is required',
-    },
-    trim: true,
-    lowercase: true,
-  },
-  dept: {
-    type: String,
-    required: {
-      value: true,
-      message: 'Dept data is not provided yet',
-    },
-    trim: true,
-    uppercase: true,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
+
+const StudentModel = mongoose.model('Student', studentSchema);
+module.exports = StudentModel;
