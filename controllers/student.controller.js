73303996583f211a09efb654b7d115fd56e87ecf
iinaamasum <iinaamasum@gmail.com>
2022-09-25 07:52:00 +0600

    all student info get request done

--- controllers/student.controller.js
+++ controllers/student.controller.js
@@ -1,12 +1,31 @@
 const StudentModel = require('../models/student.model');
-const { postStudentInfoService } = require('../services/student.service');
+const {
+  postStudentInfoService,
+  getAllStudentInfoService,
+} = require('../services/student.service');
 
 exports.getAllStudentInfo = async (req, res, next) => {
-  res.status(200).json({
-    status: 'success',
-    message: 'got all the students. @param array of objects',
-    // allStudent,
-  });
+  try {
+    const allStudentInfo = await getAllStudentInfoService();
+    if (!Array.isArray(allStudentInfo)) {
+      res.status(400).json({
+        status: 'failed',
+        message: "Can't get all students. Please populate first.",
+        allStudentInfo,
+      });
+    }
+    res.status(200).json({
+      status: 'success',
+      message: 'got all the students. @param array of objects',
+      allStudentInfo,
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: 'failed',
+      message: "Can't get all students. Server error.",
+      error,
+    });
+  }
 };
 
 exports.getStudentInfo = async (req, res, next) => {
@@ -21,7 +40,7 @@ exports.getStudentInfo = async (req, res, next) => {
 exports.postStudentInfo = async (req, res, next) => {
   try {
     const postedStudentInfo = await postStudentInfoService(req.body);
-    if (!postedStudentInfo) {
+    if (!postedStudentInfo?._id) {
       res.status(400).json({
         status: 'failed',
         message: "Can't post the student. Posting Database error.",
--- services/student.service.js
+++ services/student.service.js
@@ -1,7 +1,12 @@
 const StudentModel = require('../models/student.model');
 
 exports.postStudentInfoService = async (data) => {
-  const postDoc = StudentModel(data);
+  const postDoc = await StudentModel(data);
   const result = await postDoc.save();
+  return result;
+};
+
+exports.getAllStudentInfoService = async () => {
+  const result = await StudentModel.find();
   return result;
 };
